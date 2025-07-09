c3a7ba93325b96b8a97c777fdcbada2a51e7d1c2
choseongchan3712 <choseongchan3712@naver.com>
2025-07-09 22:02:38 +0900

    useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
+import SearchForm from "@/components/SearchForm";
 import styles from "@/styles/Home.module.css"
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h1 className={styles.title}>
-      hello next.js!
-    </h1>
 
     <div>
       <h1>Codeitmall</h1>
+      <SearchForm></SearchForm>
       <ul>
         <li>
           <Link href="/products/1">첫 번째 상품</Link>
