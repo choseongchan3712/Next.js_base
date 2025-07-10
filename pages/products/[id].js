59ea6d20e90486d5e661c5ad4e458bbc7a137391
choseongchan3712 <choseongchan3712@naver.com>
2025-07-10 16:16:46 +0900

    api

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,10 +1,39 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "@/lib/axios"
 
 const Product = () => {
+    const [product, setProduct] = useState();
+    const [sizeRiews, setSizeRiews] = useState();
     const router = useRouter();
     const {id} = router.query;
 
-    return <div>Product {id} 페이지</div>
+    async function getProduct(targetId) {
+        const res = await axios.get(`/products/${targetId}`);
+        const nextProduct = res.data;
+        setProduct(nextProduct);
+    }
+    async function getSizeRiews(targetId) {
+        const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
+        const nextSizeRiews = res.data;
+        setSizeRiews(nextSizeRiews);
+    }
+
+    useEffect(() =>{
+        if (!id) return;
+
+        getProduct(id);
+        setSizeRiews(nextSizeRiews);
+    }, [id]);
+
+    if (!product) return null;
+
+    return (
+        <div>
+            <h1>{product.name}</h1>
+            <img src={product.imgUrl} alt={product.name} />
+        </div>
+    )
 }
 
 export default Product;
